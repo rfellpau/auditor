d5ef61b925c1a8a617ec61d761a25145ac518459
rfellpau <rfellpau@amazon.com>
2025-04-05 21:59:56 +0100

    Modified styles in NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -14,15 +14,17 @@ function App() {
 
   return (
     <>
-      <nav className="bg-body-tertiary border-bottom navbar navbar-expand-lg">
+      <nav className="bg-body-tertiary border-bottom navbar">
         <div className="container">
+          <div className="align-items-center d-flex flex-grow-1">
             <span className="fw-bold mb-0 navbar-brand">
                 <span className="me-2">Auditor</span>
                 <svg xmlns="http://www.w3.org/2000/svg" width="32" height="32" fill="currentColor" className="bi bi-truck" viewBox="0 0 16 16">
                     <path d="M0 3.5A1.5 1.5 0 0 1 1.5 2h9A1.5 1.5 0 0 1 12 3.5V5h1.02a1.5 1.5 0 0 1 1.17.563l1.481 1.85a1.5 1.5 0 0 1 .329.938V10.5a1.5 1.5 0 0 1-1.5 1.5H14a2 2 0 1 1-4 0H5a2 2 0 1 1-3.998-.085A1.5 1.5 0 0 1 0 10.5zm1.294 7.456A2 2 0 0 1 4.732 11h5.536a2 2 0 0 1 .732-.732V3.5a.5.5 0 0 0-.5-.5h-9a.5.5 0 0 0-.5.5v7a.5.5 0 0 0 .294.456M12 10a2 2 0 0 1 1.732 1h.768a.5.5 0 0 0 .5-.5V8.35a.5.5 0 0 0-.11-.312l-1.48-1.85A.5.5 0 0 0 13.02 6H12zm-9 1a1 1 0 1 0 0 2 1 1 0 0 0 0-2m9 0a1 1 0 1 0 0 2 1 1 0 0 0 0-2"/>
                 </svg>
             </span>
-            <form className="d-flex flex-grow-1" role="search">
+            <form className="flex-grow-1" role="search">
+              <div class="input-group input-group-lg">
                 <input 
                 className="form-control" 
                 type="search" 
@@ -30,7 +32,9 @@ function App() {
                 value={searchQuery}
                 onChange={(event) => setSearchQuery(event.target.value.toUpperCase())}
                 />
+              </div>
             </form>
+          </div>
         </div>
       </nav>
 
